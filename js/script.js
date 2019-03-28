fec2b7f28a6085502b00ff07dcb11ed4c546a598
MAbdurahman <mdbdrrhm@gmail.com>
2019-03-28 09:45:13 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/**
+ * The Beating Heart With Text 2 Script Sheet
+ * @author:  Mahdi Abdurrahman
+ * @date:  28 March 2019
+ * @version:  1.0.0
+ */
+$(function () {
+    var typed5;
+    typed5 = new Typed('.typing', {
+
+        strings: ['I cannot get YOU off my mind!',
+            'I love everything about YOU!',
+            'I have been working to start a',
+            'love flame with YOU.',
+            'I know you are not good for me, ',
+            'but I love YOU, anyway&nbsp;&nbsp;&#9892;'
+        ],
+        typeSpeed: 120,
+        backSpeed: 70,
+        backDelay: 3000,
+        // cursorChar: '_',
+        showCursor: false,
+        shuffle: false,
+        smartBackspace: true,
+        loop: true,
+        loopCount: 3
+
+    });
+
+});
